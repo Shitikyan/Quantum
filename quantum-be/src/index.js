58e158612d85c0040b44a105fe38df7154b6ca61
shitikyan <shitikyan@gmail.com>
2023-11-21 19:27:30 +0400

    changed cors issue

--- quantum-be/src/index.js
+++ quantum-be/src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://quantum-gules.vercel.app",
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
